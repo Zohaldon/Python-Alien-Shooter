2403b9ff8a51cf8f5d6720e4bc26d04b582d0362
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2020-05-16 23:09:59 -0400

    refactored check_events

--- game_functions.py
+++ game_functions.py
@@ -1,6 +1,24 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    """Respond to key press"""
+    # Move right
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    # Move left
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Respond to key release"""
+    # Stop moving right
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    # Stop moving left
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """ 
     Catch and respond to any event caused in game
@@ -11,19 +29,9 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            # Move right
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            # Move left
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            # Stop moving right
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            # Stop moving left
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keydown_events(event, ship)
 
 def update_screen(as_settings, screen, ship):
     # add color to the screen
