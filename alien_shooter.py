c29a3a3251abb5ffa8c397b30edcaf5dde487e73
Zohaldon <47535654+Zohaldon@users.noreply.github.com>
2020-05-16 21:29:17 -0400

    added background color

--- alien_shooter.py
+++ alien_shooter.py
@@ -7,13 +7,17 @@ def run_game():
     screen = pygame.display.set_mode((1050,700))
     pygame.display.set_caption("Alien Shooter")
 
+    # background color
+    bg_color = (41, 40, 40)
+
     # start game
     while True:
         # catch keyboard and mouse event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        
+        # add color to the screen
+        screen.fill(bg_color)    
         # make screen visible
         pygame.display.flip()
 
